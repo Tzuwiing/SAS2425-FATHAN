ab0e6b2886cc6fd1f5fdcede93f316db3e68ec78
Jusuf Fathan Nuradly <jusuf1909@student.abudzar.sch.id>
2025-03-06 09:01:29 +0700

    selesai

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Project from "./projek/praktek/path.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Project />
+  </StrictMode>
+);
